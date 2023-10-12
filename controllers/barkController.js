ad3dafacd917dd9003f882d09e55657ad5b0d613
RyanmjDev <Ryanmj.dev@gmail.com>
2023-10-12 13:14:43 -0500

    Refactors barkController into barkServices

--- controllers/barkController.js
+++ controllers/barkController.js
@@ -5,8 +5,6 @@ const jwt = require('jsonwebtoken');
 
 const Notification = require("../models/notification");
 
-const handleNotification = require('../handlers/handleNotification');
-const {socketHandler, unreadNotificationCounts,  getConnectedUsers} = require('../handlers/socketHandler');
 const getUserId = require('../utils/getUserId');
 const barksPageLimit = require('../utils/barkUtils')
 
@@ -42,34 +40,7 @@ exports.createBark = async (req, res) => {
 
 exports.postReply = async (req, res) => {
   try {
-    const parentBarkId = req.params.barkId;
-    const parentBark = await Bark.findById(parentBarkId);
-
-    if (!parentBark) {
-      return res.status(404).json({ message: 'Bark not found' });
-    }
-
-    const userId = req.user.id;
-    const content = req.body.content;
-
-    if (!content) {
-      return res.status(400).json({ message: 'Content is required' });
-    }
-
-    const reply = new Bark({
-      user: userId,
-      content,
-      parentBark: parentBarkId
-    });
-
-
-
-    await reply.save();
-
-    parentBark.replies.push(reply);
-    await parentBark.save();
-
-    const barkOwner = await User.findById(parentBark.user); // Find the owner of the bark to send them a notification
+    const reply = await barkService.postReply(req.params.barkId, req.user.id, req.body.content);
 
     // const replyNotification = new Notification({
     //   user: barkOwner._id,
@@ -92,28 +63,7 @@ exports.postReply = async (req, res) => {
 exports.getReplies = async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
-
-    const parentBarkId = req.params.barkId;
-    const parentBark = await Bark.findById(parentBarkId)
-    .populate('replies')
-    .populate('user', 'username displayName')
-    .skip((page - 1) * barksPageLimit)
-    .limit(barksPageLimit)
-    .exec();
-    
-
-    if (!parentBark) {
-      return res.status(404).json({ message: 'Bark not found' });
-    }
-
-    // Take populate replies and populates the username field on them...
-    const populatedReplies = await Promise.all(
-      parentBark.replies.map(async (reply) => {
-        return await Bark.populate(reply, { path: 'user', select: 'username displayName' });
-      })
-    );
-
-
+    const populatedReplies = await barkService.getReplies(req.params.barkId, page);
     res.status(200).json(populatedReplies);
   } catch (error) {
     console.error('Error fetching replies:', error);
@@ -124,39 +74,9 @@ exports.getReplies = async (req, res) => {
 
 exports.getBarkById = async (req, res) => {
   try {
-    const barkId = req.params.barkId;
-    const bark = await Bark.findById(barkId)
-    .populate('user', 'username displayName') 
-    .exec();
-
-    if (!bark) {
-      return res.status(404).json({ message: 'Bark not found' });
-    }
-   
-if (req.headers.authorization) {
-  
-      const token = req.headers.authorization.split(" ")[1];
-    if (token) {
-      try {
-        const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
-        const userId = decodedToken.id;
-
-        if (userId) {
-
-           const user = await User.findById(userId);
-           const isLikedByUser = user.likedBarks.includes(barkId);
-           const barkWithLikes = {...bark._doc, isLikedByUser};
-           res.status(200).json(barkWithLikes);
-        }
-      } catch (error)
-      {
-        console.error('Error with token:', error);
-        res.status(500).json({ message: 'Error with token:' });
-      }
-    }
-  } else {
-    res.json(bark);
-  }
+    const token = authUtils.getTokenFromHeaders(req.headers.authorization);
+    const bark = await barkService.getBarkById(req.params.barkId, token);
+    res.status(200).json(bark);
   } catch (error) {
     console.error('Error fetching bark by ID:', error);
     res.status(500).json({ message: 'Error fetching bark by ID' });
@@ -164,74 +84,22 @@ if (req.headers.authorization) {
 };
 
 
-
 exports.likeBark = async (req, res) => {
   try {
-
-    const userId = getUserId(req.headers.authorization)
-
-    const barkId = req.params.barkId;
-
-    const bark = await Bark.findById(barkId);
-    if (!bark) {
-      return res.status(404).json({ message: 'Bark not found' });
-    }
-
-    const likeIndex = bark.likes.findIndex((like) => like.user.toString() === userId);
-    const user = await User.findById(userId);
-
-    const barkOwner = await User.findById(bark.user);
-
-    const connectedUsers = getConnectedUsers();
-    console.log("connectedUsers:", connectedUsers); // Get rid of this later
-
-    if (likeIndex === -1) {
-      // If a bark isn't liked, like it
-      bark.likes.push({ user: userId });
-      user.likedBarks.push(bark);
-      await handleNotification(userId, barkId, barkOwner, 'like', connectedUsers);
-    } else {
-      // If has already been liked, unlike it
-      const barkIndex = user.likedBarks.findIndex((likedBark) => likedBark._id.toString() === barkId);
-      if (barkIndex !== -1) {
-        user.likedBarks.splice(barkIndex, 1);
-      }
-      bark.likes.splice(likeIndex, 1);
-      await handleNotification(userId, barkId, barkOwner, 'unlike', connectedUsers);
-    }
-
-    await user.save();
-    await bark.save();
+    const userId = getUserId(req.headers.authorization);
+    const bark = await barkService.likeBark(req.params.barkId, userId);
     res.status(200).json(bark);
-    } catch (error) {
+  } catch (error) {
     console.error('Error liking bark:', error);
     res.status(500).json({ message: 'Error liking bark', error });
-    }
+  }
 };
 
-
 exports.deleteBark = async (req, res) => {
-  const { barkId } = req.params;
-
-  if (!req.user) {
-    return res.status(401).json({ message: 'Unauthorized' });
-  }
-
   try {
-    const bark = await Bark.findById(barkId);
-
-    if (!bark) {
-      return res.status(404).json({ message: 'Bark not found' });
-    }
-
-    if (bark.user.toString() !== req.user._id.toString()) {
-      return res.status(403).json({ message: 'Forbidden' });
-    }
-
-    await Bark.findByIdAndDelete(barkId);
+    await barkService.deleteBark(req.params.barkId, req.user._id);
     res.status(200).json({ message: 'Bark deleted successfully' });
   } catch (err) {
     res.status(500).json({ message: 'Error deleting bark', error: err });
   }
 };
-
--- services/barkService.js
+++ services/barkService.js
@@ -2,6 +2,8 @@ const Bark = require('../models/bark');
 const User = require('mongoose').model('User');
 const jwt = require('jsonwebtoken'); 
 
+const {socketHandler, unreadNotificationCounts,  getConnectedUsers} = require('../handlers/socketHandler');
+const handleNotification = require('../handlers/handleNotification');
 
 const barkUtils = require('../utils/barkUtils');
 
@@ -38,6 +40,113 @@ exports.createBark = async(userId, content) => {
     });
   } catch (error) {
     console.error('Error creating bark:', error);
+    throw error;
+  }
+};
+
+exports.postReply = async (parentBarkId, userId, content) => {
+  try {
+    const parentBark = await Bark.findById(parentBarkId);
+    if (!parentBark) throw new Error('Bark not found');
+
+    const reply = new Bark({
+      user: userId,
+      content,
+      parentBark: parentBarkId,
+    });
+    await reply.save();
+    parentBark.replies.push(reply);
+    await parentBark.save();
+    return reply;
+  } catch (error) {
+    console.error('Error posting reply:', error);
+    throw error;
+  }
+}
+
+
+exports.getReplies = async (parentBarkId, page) => {
+  try {
+    const parentBark = await Bark.findById(parentBarkId)
+      .populate('replies')
+      .populate('user', 'username displayName')
+      .skip((page - 1) * barkUtils.barksPageLimit)
+      .limit(barkUtils.barksPageLimit)
+      .exec();
+    if (!parentBark) throw new Error('Bark not found');
+
+    return await Promise.all(
+      parentBark.replies.map(async (reply) => {
+        return await Bark.populate(reply, { path: 'user', select: 'username displayName' });
+      })
+    );
+  } catch (error) {
+    console.error('Error fetching replies:', error);
+    throw error;
+  }
+};
+
+exports.getBarkById = async (barkId, token) => {
+  try {
+    const bark = await Bark.findById(barkId).populate('user', 'username displayName').exec();
+    if (!bark) throw new Error('Bark not found');
+
+    if (token) {
+      const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+      const userId = decodedToken.id;
+      if (userId) {
+        const user = await User.findById(userId);
+        const isLikedByUser = user.likedBarks.includes(barkId);
+        return { ...bark._doc, isLikedByUser };
+      }
+    }
+    return bark;
+  } catch (error) {
+    console.error('Error fetching bark by ID:', error);
+    throw error;
+  }
+};
+
+exports.likeBark = async (barkId, userId) => {
+  try {
+    const bark = await Bark.findById(barkId);
+    if (!bark) throw new Error('Bark not found');
+
+    const likeIndex = bark.likes.findIndex((like) => like.user.toString() === userId);
+    const user = await User.findById(userId);
+    const barkOwner = await User.findById(bark.user);
+    const connectedUsers = getConnectedUsers();
+
+    if (likeIndex === -1) {
+      bark.likes.push({ user: userId });
+      user.likedBarks.push(bark);
+      await handleNotification(userId, barkId, barkOwner, 'like', connectedUsers);
+    } else {
+      const barkIndex = user.likedBarks.findIndex((likedBark) => likedBark._id.toString() === barkId);
+      user.likedBarks.splice(barkIndex, 1);
+      bark.likes.splice(likeIndex, 1);
+      await handleNotification(userId, barkId, barkOwner, 'unlike', connectedUsers);
+    }
+
+    await user.save();
+    await bark.save();
+    return bark;
+  } catch (error) {
+    console.error('Error liking bark:', error);
+    throw error;
+  }
+};
+
+exports.deleteBark = async (barkId, userId) => {
+  try {
+    const bark = await Bark.findById(barkId);
+    if (!bark) throw new Error('Bark not found');
+    if (bark.user.toString() !== userId.toString()) throw new Error('Forbidden');
+
+    await Bark.findByIdAndDelete(barkId);
+    return;
+  } catch (error) {
+    console.error('Error deleting bark:', error);
     throw error;
   }
 };
